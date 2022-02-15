0d4b4b97e90097fffe45814e9c0ac7ed9cedcd14
ThakarKrishna <krishnathakar2001@gmail.com>
2022-02-15 10:13:53 +0530

    modified

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -20,34 +20,39 @@ const getDatafromLS = () => {
 const Registration = () => {
 
     const [isFormSubmitted, setFormSubmitted] = useState(false);
-    const [femail,setFemail]=useState(false);
-    const [fpassword,setFpassword]=useState(false);
+    // const [femail,setFemail]=useState(false);
+    // const [fpassword,setFpassword]=useState(false);
 
-    const [fphone,setFphone]=useState(false);
+    // const [fphone,setFphone]=useState(false);
 
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [login, setLogin] = useState(true);
+    const [flag, setFlag] = useState(false);
+
     const [phone, setPhone] = useState("");
     const [details, setDetails] = useState(getDatafromLS());
-    const [formValue, setFormValue] = useState({
-        userName: '',
-        email:'',
-        password:'',
-        phone:''
-    });
+//     const [formValue, setFormValue] = useState({
+//         userName: '',
+//         email:'',
+//         password:'',
+//         phone:''
+//         // getDatafromLS()
+// });
+    
 
 
 
 
 
     const onSubmit = (e) => {
+        
         e.preventDefault();
         setFormSubmitted(true);
-        setFemail(true);
-        setFpassword(true);
-        setFphone(true)
+        // setFemail(true);
+        // setFpassword(true);
+        // setFphone(true)
       
         
 
@@ -58,29 +63,36 @@ const Registration = () => {
             phone
 
         }
-        setDetails([...details, detail]);
-        setUsername("");
-        setEmail("");
-        setPassword("");
-        setPhone("");
+        
+       
+        // localStorage.setItem('details', JSON.stringify(details));
 
+if(!username || !email || !password ||!phone){
+    setFlag(true);
 
+}
+else{
+    setDetails([...details,detail]);
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setPhone("");
 
+}
 
-
-
+console.log(details)
 
 
 
     }
 
 
-    const handleChange = (e) => {
-        console.log("jhjhbjhbjb",e.target.name);
-        setFormValue({
-            [e.target.name]: e.target.value
-        })
-    }
+    // const handleChange = (e) => {
+    //     console.log("jhjhbjhbjb",e.target.name);
+    //     // setFormValue({
+    //     //     [e.target.name]: e.target.value
+    //     // })
+    // }
 
     const handleClick = () => {
         setLogin(!login);
@@ -103,10 +115,10 @@ const Registration = () => {
 
                         <Form.Group className="mb-3" controlId="formBasicEmail" aria-autocomplete='off'>
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" name="userName" onChange={handleChange} placeholder="Enter Username"
+                            <Form.Control type="text" name="userName" onChange={(e)=>setUsername(e.target.value)} placeholder="Enter Username"
                             /><br/>
                             {
-                                isFormSubmitted && formValue.userName === "" &&
+                                isFormSubmitted && details.username == "" &&
                                 <Alert color="primary" variant="danger">Username is required</Alert>
                                 
                             }
@@ -116,35 +128,36 @@ const Registration = () => {
                         <Form.Group className="mb-3" controlId="Email" >
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" name="email" 
-                            onChange={handleChange} placeholder="Enter email" />
+                            onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
                             <br/>
-                            {
+                            {/* {
                                 femail && formValue.email === "" &&
                                 <Alert color="primary" variant="danger">E-mail is required</Alert>
-                            }
+                            } */}
                             
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="Password">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password"  name="password"
-                            onChange={handleChange} placeholder="Password"  />
+                            onChange={(e)=>setPassword(e.target.value)} placeholder="Password"  />
                             <br/>
-                            {
+                            {/* {
                                 fpassword && formValue.password === "" &&
                                 <Alert color="primary" variant="danger">password is required</Alert>
-                            }
+                            } */}
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Phone no.</Form.Label>
                             <Form.Control type="number" name="phone"
-                            onChange={handleChange}  placeholder="Enter phone no."  /><br/>
+                            onChange={(e)=>setPhone(e.target.value)}  placeholder="Enter phone no."  /><br/>
                             
-                            {
+
+                            {/* {
                                fphone && formValue.phone === "" &&
                                 <Alert color="primary" variant="danger">Phone number is required</Alert>
-                            }
+                            } */}
                         </Form.Group>
 
                         <Button variant="dark" type="submit" 
@@ -162,10 +175,10 @@ const Registration = () => {
                             Already registered{" "}log in?
 
                         </p>
-                        {/* {flag && (
+                        {flag && (
                             <Alert color="primary" variant="danger">
                                 Every Field is important !
-                            </Alert>)} */}
+                            </Alert>)}
                     </Form>
 
 
